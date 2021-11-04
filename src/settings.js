2743a615585cccec97094ee529f426c4e063a582
itheima <itheima@itcast.cn>
2021-11-04 16:05:49 +0800

    人资项目初始化

---
+++ src/settings.js
@@ -0,0 +1,18 @@
+module.exports = {
+    // 项目信息配置
+
+    // 网页标题
+    title: "Vue Admin Template",
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether fix the header
+     */
+    fixedHeader: false,
+
+    /**
+     * @type {boolean} true | false
+     * @description Whether show the logo in sidebar
+     */
+    sidebarLogo: false,
+};
